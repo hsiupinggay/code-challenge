5625c3ae40432bd2ddc2c9669a5fa502d3c3cb58
Hsiu Ping Gay <hsiups@Hsius-MacBook-Pro.local>
2022-04-27 15:05:58 -0700

    problem 4 completed

--- src/problem4/index.js
+++ src/problem4/index.js
@@ -1,1 +1,65 @@
-console.log("hello moon");
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var axios_1 = require("axios");
+var ethers_1 = require("ethers");
+// replace BNB_API_KEY with own API key
+var BNB_API_KEY = "";
+var addresses = [
+    "0x123d475e13aa54a43a7421d94caa4459da021c77",
+    "0x0020c5222a24e4a96b720c06b803fb8d34adc0af",
+    "0xfe808b079187cc460f47374580f5fb47c82b87a5",
+];
+var contractAddress = "0x250b211ee44459dad5cd3bca803dd6a7ecb5d46c";
+var getBalance = function (addresses, contractAddress) {
+    addresses.forEach(function (address) { return __awaiter(void 0, void 0, void 0, function () {
+        var res, result, formatedBalance;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, axios_1["default"].get("https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=".concat(contractAddress, "&address=").concat(address, "&tag=latest&apikey=").concat(BNB_API_KEY))];
+                case 1:
+                    res = _a.sent();
+                    result = res.data.result;
+                    formatedBalance = ethers_1.ethers.utils.formatEther(result);
+                    console.log("".concat(address, "  ").concat(formatedBalance));
+                    return [2 /*return*/];
+            }
+        });
+    }); });
+};
+getBalance(addresses, contractAddress);
--- src/problem4/index.ts
+++ src/problem4/index.ts
@@ -1,5 +1,26 @@
+import axios from "axios";
 import { ethers } from "ethers";
 
-const provider = new ethers.providers.JsonRpcProvider(
-  `https://api.bscscan.com/api?module=account&action=balance&address=0x70F657164e5b75689b64B7fd1fA275F334f28e18&apikey=${BNB_API_KEY}`
-);
+// replace BNB_API_KEY with own API key
+const BNB_API_KEY = "";
+
+const addresses = [
+  "0x123d475e13aa54a43a7421d94caa4459da021c77",
+  "0x0020c5222a24e4a96b720c06b803fb8d34adc0af",
+  "0xfe808b079187cc460f47374580f5fb47c82b87a5",
+];
+
+const contractAddress = "0x250b211ee44459dad5cd3bca803dd6a7ecb5d46c";
+
+const getBalance = (addresses, contractAddress) => {
+  addresses.forEach(async (address) => {
+    const res = await axios.get(
+      `https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${contractAddress}&address=${address}&tag=latest&apikey=${BNB_API_KEY}`
+    );
+    const { result } = res.data;
+    const formatedBalance = ethers.utils.formatEther(result);
+    console.log(`${address}  ${formatedBalance}`);
+  });
+};
+
+getBalance(addresses, contractAddress);
